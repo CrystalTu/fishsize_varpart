93b46ea1b87627a172d2e96abc8e4aae2c05b17c
Chen-Yi Tu <tu.chenyi@gmail.com>
2017-09-29 20:08:18 +0800

    automatically create the output folder in do.R

---
+++ do.R
@@ -0,0 +1,21 @@
+# Run the analysis
+# Crystal Tu 20170929
+
+setwd("~/Desktop/fishsize_varpart/")
+dir.create("~/Desktop/fishsize_varpart/output")
+
+# Variation partitioning
+source("~/Desktop/fishsize_varpart/script/variation_partitioning.r")
+# Combine all time lag & LM on explained fraction of fishing/temperature v.s. Lifehist
+source("~/Desktop/fishsize_varpart/script/meta_variation_partitioning.R")
+
+# Univariate SBIs
+source("~/Desktop/fishsize_varpart/script/univariateSBI.R")
+source("~/Desktop/fishsize_varpart/script/meta_uniSBIs.R")
+
+# Summary plot + ANOVA test
+source("~/Desktop/fishsize_varpart/script/summary.R")
+
+# Comparison plot + binomial test
+source("~/Desktop/fishsize_varpart/script/comparison.R")
+
